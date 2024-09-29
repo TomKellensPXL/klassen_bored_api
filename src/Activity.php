ef75b69a57df91ed78808eb3ce90d49a7d53b141
unknown <12002655@student.pxl.be>
2024-09-30 00:39:55 +0200

    chalk feature added

--- src/Activity.php
+++ src/Activity.php
@@ -2,6 +2,7 @@
 //namespace src;
 
 use GuzzleHttp\Client;
+use TdTrung\Chalk\Chalk;
 class Activity
 {
     private Client $client;
@@ -11,10 +12,12 @@ class Activity
     }
 
     public function getActivity(): string {
+        $chalk = new Chalk();
         $res = $this->client->request('GET', 'https://bored-api.appbrewery.com/random');
         $data = json_decode($res->getBody(), true);
         $activity = $data["activity"]; //Ik heb nog nooit zoiets gezien in mijn leven
-
-        return $activity ?? 'No activity available';
+//        $activity = null;
+//        return $chalk->green($activity) ?? $chalk->blue->underscore('No activity available'); //CHALK GEEFT ALTIJD EEN STRING TERUG. Ik weet niet of dat sowieso al is als de json property leeg is.
+        return !empty($activity) ? $chalk->green($activity) : $chalk->blue->underscore('No activity available');
     }
 }
